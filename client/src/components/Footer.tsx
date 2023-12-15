76903ef8125394395cb304e4832e802eb061c2ec
woodyRocket <ttr.nguy+github@gmail.com>
2023-12-15 22:45:33 +0700

    change footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,31 +1,41 @@
-import React from 'react'
-import { Icon } from "@iconify/react"
+import React from "react";
+import { Icon } from "@iconify/react";
 
 const Footer = () => {
   return (
-    <footer className='h-fit bg-[#7ED7C1] flex items-center text-[#333] py-6'>
+    <footer className="h-fit bg-[#7ED7C1] flex items-center text-[#333] py-6">
       <div className="footer_left_panel flex flex-col w-2/3">
-        <img src="/smartsocket.png" alt="" className='grow-0 max-h-24 w-fit scale-50 -translate-x-24'/>
-        <div className="placeholder-text w-2/3 translate-x-20 mb-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum odio minima officia? Eveniet perferendis minus magni architecto voluptatem tenetur dolor rerum dicta iure, qui quis similique quo distinctio, quia hic.</div>
+        <img
+          src="/smartsocket.png"
+          alt=""
+          className="grow-0 max-h-24 w-fit scale-50 -translate-x-24"
+        />
+        <div className="placeholder-text w-2/3 translate-x-20 mb-6">
+          The smart power socket, with an RS-485 connected power measuring unit
+          and a controller board, calculates and displays real-time power usage.
+          It also features internet connectivity for remote monitoring via a web
+          interface.
+        </div>
         <div className="social-icons flex text-3xl gap-2 translate-x-20">
-          <Icon icon="mdi:facebook-box" className='cursor'/>
-          <Icon icon="mdi:instagram" className='cursor'/>
-          <Icon icon="mdi:twitter" className='cursor'/>
-          <Icon icon="mdi:reddit" className='cursor'/>
+          <Icon icon="mdi:facebook-box" className="cursor" />
+          <Icon icon="mdi:instagram" className="cursor" />
+          <Icon icon="mdi:twitter" className="cursor" />
+          <Icon icon="mdi:reddit" className="cursor" />
         </div>
       </div>
       <div className="footer_right_panel">
-        <ul className='flex flex-col gap-4'>
-          <li className='text-2xl font-semibold'>
-            Contact
-          </li>
+        <ul className="flex flex-col gap-4">
+          <li className="text-2xl font-semibold">Contact</li>
           <li>Telephone: (+84) 123456789</li>
           <li>Email: smartsocket@gmail.com</li>
-          <li className='w-72'>Address: 268 Ly Thuong Kiet St, District 10, Ho Chi Minh City, Vietnam</li>
+          <li className="w-72">
+            Address: 268 Ly Thuong Kiet St, District 10, Ho Chi Minh City,
+            Vietnam
+          </li>
         </ul>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- client/src/pages/PowerDisplay.tsx
+++ client/src/pages/PowerDisplay.tsx
@@ -7,14 +7,14 @@ const PowerDisplay = () => {
   const [powerValue, setPowerValue] = useState(0);
   // const type = typeof process.env.REACT_APP_MQTT_HOST
   const MQTT_HOST = "mqtt://io.adafruit.com:1883";
-  const MQTT_USERNAME = "nquochuy137";
-  const MQTT_PASSWORD = "aio_wtwT13k6vi2GsvxMLrJp39YA3HkF";
+  const MQTT_USERNAME = "dadn222";
+  const MQTT_PASSWORD = "aio_eaEO22o3lx45leFG8AweMqs2YPJt";
 
   useEffect(() => {
     const mqttclient = new MQTTService(MQTT_HOST, MQTT_USERNAME, MQTT_PASSWORD)
 
     mqttclient.connect();
-    mqttclient.subscribe("nquochuy137/feeds/nest-test");
+    mqttclient.subscribe("dadn222/feeds/voltage");
     console.log(mqttclient.messageCallback())
   })
 
